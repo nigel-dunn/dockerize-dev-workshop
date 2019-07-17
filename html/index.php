3175165645448679d919b82433d9ad526fcadb9c
Nigel Dunn <nigel@redefine.co.uk>
2019-07-17 12:52:46 +0100

    Add database access in Slim

--- html/index.php
+++ html/index.php
@@ -12,4 +12,19 @@ $app->get('/hello/{name}', function (Request $request, Response $response, array
 
     return $response;
 });
+$app->get('/events/', function (Request $request, Response $response, array $args) {
+    $connection = new \mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+    $connection->select_db($_ENV['DB_NAME']);
+    $result = $connection->query("SELECT * FROM events ORDER BY id");
+
+    $response->getBody()->write("<ul>");
+    while ($event = $result->fetch_assoc()) {
+        $response->getBody()->write("<li>{$event['name']}</li>");
+    }
+    $response->getBody()->write("</ul>");
+
+    $connection->close();
+
+    return $response;
+});
 $app->run();
